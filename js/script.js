9244d9b6f98e61ddbb87820544bde67b68ffbba4
Ahmed Tarek <ahmedmasr262@gmail.com>
2024-03-03 22:31:42 +0200

    Revert "Merge branch 'master' of https://github.com/Mohamed20a/My-doctor"
    This reverts commit 8a59cd4ee3bf1c8771a518e351f8b9c59bbf02d9, reversing
    changes made to fb7bfff7c157a69e40569ab628cba2b05786cb03.

--- js/script.js
+++ js/script.js
@@ -99,19 +99,3 @@ let choices = [
         scrollBtn.classList.add('active') :
         scrollBtn.classList.remove('active'); 
     }
-
-
-    // Form One
-    function nextStep() {
-
-        document.getElementById('step1').style.display = 'none';
-
-        document.getElementById('step2').style.display = 'block';
-    }
-    // Form Tow
-    function previousStep() {
-
-        document.getElementById('step2').style.display = 'none';
-
-        document.getElementById('step1').style.display = 'block';
-    }
